ac5c6669bffa20a105ccddf14b7e9d6751a21d86
RADHA RAMAN TIWARI <70423309+tiwarirr@users.noreply.github.com>
2022-12-25 13:51:12 +0000

    abc

---
+++ JS_Totorial/variables.js
@@ -0,0 +1,7 @@
+const accountId=14453
+let accountEmail="abc@gmail.com"
+var accountPassword="1234"
+accountCity = "Jainpur"
+// accountId=2
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword])
